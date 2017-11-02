0d12d5e153267a190116b8433884a2b36f5d697d
timusmc96 <timusmc96@yahoo.com>
2017-11-02 14:54:26 +0000

    Update

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,9 @@
 ##
 ## Copyright:: 2017, The Authors, All Rights Reserved.
 #
-
+      package 'httpd' do
+        action :install
+      end
 
       template '/var/www/html/index.html' do
         source 'index.html.erb'
